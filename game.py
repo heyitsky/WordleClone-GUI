ae06bf741e32eabe914085f5c5556b7c2626b829
Ky <p.kystaal@gmail.com>
2024-06-06 22:00:18 +1000

    added reset, display_state, get_word and message functions for play()

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 #implement error handling (can only use letters)
 from string import ascii_letters
+import random
 
 ASCII = str(ascii_letters)
 MAX_NUMBER_OF_GUESSES = 5
-GUESSES = {"incorrect": "🔴", "correct": "🟢", "partially": "🟠"}
+GUESSES = {"incorrect": "🔴", "correct": "🟢", "partially": "🟠", "unknown": "?"}
 
 class Game:
     """A simple wordle clone"""
@@ -13,7 +14,7 @@ class Game:
         self.guesses = 0
         self.guess_list = []
         self.current_word = "test"
-        self.current_pattern = "-----"
+        self.guess_pattern = ["-----"]
         self.game_over = False
     
     def guess(self):
@@ -31,3 +32,34 @@ class Game:
             self.guess_list.append(guess)
             return guess
 
+    def play(self):
+        if not self.game_over:
+            self.reset()
+        self.current_word = self.get_game_word()
+        print(self.current_word)
+        self.start_message()
+
+    def start_message(self):
+        input("Ready to play a new game, press ENTER/RETURN to start..!")
+    
+    def reset(self):
+        self.guesses = 0
+        self.guess_list = []
+        self.guess_pattern = ["-----"]
+        self.game_over = False
+
+    def get_game_word(self):
+        with open('wordbank.txt', 'r') as wordbank:
+            word = random.choice(wordbank.read().split(','))
+            return word
+    
+    def display_state(self):
+        print('Guesses:')
+        print('------------')
+        for i in len(self.guesses):
+            print(' '.join(self.guess_list[i]))
+            print(self.guess_pattern[i])
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
